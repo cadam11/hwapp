d07f4b74b8b4e2bab177a626a427a70ee6a586b7
Craig Adam <craig@adam11.ca>
2016-06-16 16:10:54 -0500

    Add static files functionality

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ namespace WebApplication
     {
         public void Configure(IApplicationBuilder app)
         {
+            app.UseStaticFiles();
             app.Run(context =>
             {
                 return context.Response.WriteAsync("Hello from ASP.NET Core!");
